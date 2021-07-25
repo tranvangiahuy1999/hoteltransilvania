d54da34004443cdb6cc3a7adfd27232b761fe254
Gia Huy <tranvangiahuy1999@gmail.com>
2021-07-25 17:17:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'));
+
+app.get('/login', (req, res) => {
+    res.render('./login.ejs')
+})
+
+app.get('/main', (req, res) => {
+    res.render('./main.ejs')
+})
+
+app.get('/about', (req, res) => {
+    res.render('./about.ejs')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('./contact.ejs')
+})
+
+app.get('/faq', (req, res) => {
+    res.render('./faq.ejs')
+})
+
+app.get('/index', (req, res) => {
+    res.render('./index.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('./register.ejs')
+})
+
+app.get('/renter', (req, res) => {
+    res.render('./renter.ejs')
+})
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080')
+})
